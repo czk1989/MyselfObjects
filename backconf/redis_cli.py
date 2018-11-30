bd6a7f93da1fb46c3258fc12cf1a12549b55be15
chaizhikuan <399578426@qq.com>
2018-12-01 01:36:47 +0800

    move the redis pwd

--- backconf/redis_cli.py
+++ backconf/redis_cli.py
@@ -3,7 +3,7 @@ import redis
 from django.conf import settings
 def redis_conn():
     pool = redis.ConnectionPool(host=settings.REDIS_CONN['HOST'], \
-                                port=settings.REDIS_CONN['PORT'],db=settings.REDIS_CONN['DB'],password='meiyoumima')
+                                port=settings.REDIS_CONN['PORT'],db=settings.REDIS_CONN['DB'],password=settings.REDIS_CONN['PASSWORD'])
     r = redis.Redis(connection_pool=pool)
     return  r
 
